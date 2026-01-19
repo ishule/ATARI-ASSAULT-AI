8dc48a95152c440424da3859bca44e2c64d33fbd
Daniel <espinosacanovasdaniel@gmail.com>
2026-01-19 18:16:57 +0100

    entrega

---
+++ build/CMakeFiles/sharedLibraryInterfaceExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/joderdanie/Desktop/RA/Arcade-Learning-Environment-0.6.1/lib/ale/doc/examples/ale-sharedLibraryInterfaceExample"
+  "/home/joderdanie/Desktop/RA/Arcade-Learning-Environment-0.6.1/lib/ale/doc/examples/ale-sharedLibraryInterfaceExample.pdb"
+  "CMakeFiles/sharedLibraryInterfaceExample.dir/doc/examples/sharedLibraryInterfaceExample.cpp.o"
+  "CMakeFiles/sharedLibraryInterfaceExample.dir/doc/examples/sharedLibraryInterfaceExample.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sharedLibraryInterfaceExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sharedLibraryInterfaceExample.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for sharedLibraryInterfaceExample.
+# This may be replaced when dependencies are built.
